1375d6a36f3277be1ee98bccf59ba202082363b8
Chathura Deshapriya <57343967+chathuradeshapriya@users.noreply.github.com>
2022-08-27 01:21:04 +0530

    Finished Splash screen

---
+++ lib/world_status.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class WorldStatus extends StatefulWidget {
+  const WorldStatus({Key? key}) : super(key: key);
+
+  @override
+  State<WorldStatus> createState() => _WorldStatusState();
+}
+
+class _WorldStatusState extends State<WorldStatus> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
